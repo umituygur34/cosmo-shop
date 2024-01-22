71448e2bee3d64d8eace96a1a602c87d7e1829bb
umituygur20 <umituygur34@gmail.com>
2024-01-22 22:26:21 +0300

    initialCommit2

--- src/app/(main)/contact/page.jsx
+++ src/app/(main)/contact/page.jsx
@@ -1,38 +1,35 @@
 import { wait } from "@/utils/misc";
 import Image from "next/image";
 import React from "react";
-import logo from "../../../../public/images/logo.png"
+import logo from "../../../../public/images/logo.png";
 import PageHeader from "@/components/common/page-header";
 
 export const metadata = {
-	title: "Contact",
-	description: "You can get luxury electornic devices",
+  title: "Contact",
+  description: "You can get luxury electornic devices",
 };
 
-
-
 const ContactPage = async () => {
-	await wait(3000);
+  await wait(3000);
 
-	console.log("Bu sayfa statik")
+  console.log("Bu sayfa statik");
 
-	return (
-		<>
+  return (
+    <>
+      <PageHeader title="Contact" />
 
-			<PageHeader title="Contact"/>
+      <div style={{ height: "200px", width: "100%", position: "relative" }}>
+        <Image
+          src="https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          fill
+          style={{ objectFit: "cover", objectPosition: "bottom center" }}
+          alt="image"
+        />
+      </div>
 
-			<div style={{height: "200px", width:"100%", position:"relative"}}>
-				<Image
-					src="https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-					fill
-					style={{objectFit: "cover", objectPosition:"bottom center"}}
-				/>
-			</div>
-
-			<Image src={logo}/>
-
-		</>
-	);
+      <Image src={logo} alt="logo" />
+    </>
+  );
 };
 
 export default ContactPage;
