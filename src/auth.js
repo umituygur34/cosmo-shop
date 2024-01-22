deb4f8d743a17b6aafcf1f6dae744bb12c2bdf4c
umituygur20 <umituygur34@gmail.com>
2024-01-22 22:37:52 +0300

    v3

--- src/auth.js
+++ src/auth.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
+import { redirect } from "next/navigation";
 
 const config = {
   providers: [
@@ -16,6 +17,7 @@ const config = {
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(credentials),
         });
+
         if (!res.ok) return null;
         const user = await res.json();
         return user;
@@ -29,9 +31,24 @@ const config = {
     // middleware icinde aktihf hale getirilen route lara her giriste calisir
     // eger bu callback true donerse route icine girlir, yoksa signin sayfasina gidilir
     authorized({ request, auth }) {
-      console.log(auth);
-      return !!auth?.user;
+      const { pathname, searchParams } = request.nextUrl;
+
+      const isUserLoggedIn = !!auth?.user;
+      const isUserInLoginPage = pathname === "/login";
+
+      if (isUserLoggedIn && isUserInLoginPage) {
+        const callbackUrl = searchParams.get("callbackUrl");
+        const url = new URL(callbackUrl || "/dashboard", request.nextUrl);
+        return Response.redirect(url);
+      }
+
+      return isUserLoggedIn;
     },
   },
+  pages: {
+    signIn: "/login",
+  },
 };
-export const { handlers, auth } = NextAuth(config);
+
+// Olusturulan config nesnesine gore tum altyapiyi hazirlayan NextAuth metodudur.
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
